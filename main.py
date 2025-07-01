8a3e9cc4809dc1aa5ff4160853d74a46180f8d42
Sachinkumar205 <SACHINCHAWAN7115@GMAIL.COM>
2025-07-01 10:57:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 arr = [12, 45, 1, 89, 45, 89, 34]
-max1 = max2 = float('-inf')
+first = second= float('-inf')
 
 for num in arr:
-    if num > max1:
-        max2 = max1
-        max1 = num
-    elif max1 > num > max2:
-        max2 = num
+    if num > first:
+        second = first
+        first = num
+    elif first> num > second:
+        second = num
 
-if max2 == float('-inf'):
+if second == float('-inf'):
     print("There is no second maximum element.")
 else:
-    print("Second maximum element is:", max2)
+    print("Second maximum element is:", second)
 
 
